ad6d0ca018fa5cb79770e259636149e5c01ca4ba
Alex Williamson <alex.williamson@crworldwide.com>
2021-02-10 17:20:41 +0000

    Scroll behaviour

--- js/heroCanvas.js
+++ js/heroCanvas.js
@@ -1,24 +1,82 @@
-(function() {
 
-    // Pts quick start mode.
-    var run = Pts.quickStart( "#pt", "#123" ); 
+var u;
+var l;
+var a;
+var mods = [];
+var x;
+var y;
+var count;
+
+function setup() {
+
+  const parent = document.querySelector('#canvasHolder');
+  let parentSize = parent.getBoundingClientRect();
+  let height = parentSize.height;
+  let width = parentSize.width;
+
+  let canvas = createCanvas(width, height);
+  canvas.parent("#canvasHolder");
+  u = 100;
+  l = 40;
+  var highCount = height/80;
+  var wideCount = width/80;
+  count = int(highCount * wideCount);
   
-    run( (time, ftime) => {
+  var index = 0;
+  for (var xc = 0; xc < wideCount; xc++) {
+    for (var yc = 0; yc < highCount; yc++) {
+      mods[index++] = new Module(int(xc)*u,int(yc)*u);
+    }
+   }
+}
+
+function draw() {
   
-      // get a line from pointer to center, and use it for direction and magnitude calculations
-      let ln = space.pointer.$subtract( space.center.$add(0.5) );
-      let dir = ln.$unit();
-      let mag = ln.magnitude();
-      let mag2 = space.size.magnitude();
   
-      // create a grid of lines
-      let lines = Create.gridPts( space.innerBound, 20, 10 ).map( (p) => {
-        let dist = p.$subtract( space.center ).magnitude() / mag2;
-        return new Group(p, p.$add( dir.$multiply( dist*(20 + mag/5) ) )) 
-      });
-      
-    
-      form.strokeOnly("#fff").lines( lines );
-    });
   
-  })();
+  if (mouseIsPressed) {
+    background(0);
+    stroke(255,163,163);
+  } else {
+    background(255,163,163);
+    stroke(255);
+  }
+  
+  strokeWeight(5);
+  
+  translate(20, 20);
+  
+  for (var i = 0; i <= count; i++) {
+    mods[i].update();
+    mods[i].draw2();
+  }
+
+}
+
+function Module(_x, _y) {
+  this.x = _x;
+  this.y = _y;
+  this.a = 0;
+  
+
+}
+
+Module.prototype.update = function() {
+  if (mouseIsPressed) {
+    this.a = -20 * (atan2(mouseY-this.y, mouseX-this.x));
+  } else {
+    this.a = atan2(mouseY-this.y, mouseX-this.x);
+  }
+}
+
+Module.prototype.draw2 = function() {
+  push();
+  translate(this.x, this.y);
+  rotate(this.a);
+  line(-l,0,l,0);
+  pop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+const navItems = [...document.querySelectorAll(".nav-item a")];
+
+function scrollHandler() {
+    navLinkColor();
+}
+
+function navLinkColor() {
+    const scrollPos = window.scrollY;
+    navItems.forEach(link => {
+        let section = document.querySelector(link.hash);
+        let parent = link.parentElement;
+
+        if (section.offsetTop <= scrollPos && section.offsetTop + section.offsetHeight > scrollPos) {
+            parent.classList.add("active");
+        } else {
+            parent.classList.remove("active");
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollHandler);
+window.addEventListener('load', scrollHandler);
